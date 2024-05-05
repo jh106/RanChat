c99a1eaf79a53e1a749348ef4b157db5e09f42d4
Wonho Jo <jwh926@proton.me>
2024-05-05 13:41:31 +0900

    chat content + navbar work
    Co-authored-by: j__h.106 <wlsghks245@gmail.com>

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,16 +2,19 @@ import "../assets/app.css";
 
 export default function Chat() {
 	return (
-    <div className="content">
-        <div className="chat_div">
-          <div className="chat_main">
-            asdf
-          </div>
-          <form className="chat_input">
-            <input type="text" />
-            <div className="chat_send_Button"><button>send</button></div>
-          </form>
-        </div>
-    </div>
+		<div className="content">
+			<div className="chat_div">
+				<div className="chat_main">
+					<p className="left">left</p>
+					<p className="right">right</p>
+				</div>
+				<form className="chat_input">
+					<input type="text" />
+					<div className="chat_send_Button">
+						<button>send</button>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
 }
--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,14 +1,22 @@
 import "../assets/app.css";
 import { NavLink } from "react-router-dom";
 
+const activeStyle = {};
+
 export default function Nav() {
-    return (
-        <div className="nav-bar">
-          <nav>
-          <NavLink to="/">home</NavLink>
-          <NavLink to="/chat">시작하기</NavLink>
-          <NavLink to="/privacy">개인정보 보호</NavLink>
-          </nav>
-        </div>
-    );
+	return (
+		<div className="nav-bar">
+			<nav className="nav">
+				<NavLink to="/" style={{ textDecoration: "none" }}>
+					<div className="nav_link">home</div>
+				</NavLink>
+				<NavLink to="/chat" style={{ textDecoration: "none" }}>
+					<div className="nav_link">시작하기</div>
+				</NavLink>
+				<NavLink to="/privacy" style={{ textDecoration: "none" }}>
+					<div className="nav_link">개인정보 보호</div>
+				</NavLink>
+			</nav>
+		</div>
+	);
 }
