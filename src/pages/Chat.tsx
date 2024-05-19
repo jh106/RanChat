288aea6296d6091b62abeb2cdd214a4e8f1f5e35
Wonho Jo <jwh926@proton.me>
2024-05-19 12:42:03 +0900

    websocket early implementation

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,19 +1,57 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 import "../assets/app.css";
 
 export default function Chat() {
+	//Public API that will echo messages sent to it back to the client
+	const [socketUrl, setSocketUrl] = useState("wss://echo.websocket.org");
+	const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>(
+		[]
+	);
+
+	const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+	useEffect(() => {
+		if (lastMessage !== null) {
+			setMessageHistory((prev) => prev.concat(lastMessage));
+		}
+	}, [lastMessage]);
+
+	const handleClickChangeSocketUrl = useCallback(
+		() => setSocketUrl("wss://demos.kaazing.com/echo"),
+		[]
+	);
+
+	const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
+
+	const connectionStatus = {
+		[ReadyState.CONNECTING]: "Connecting",
+		[ReadyState.OPEN]: "Open",
+		[ReadyState.CLOSING]: "Closing",
+		[ReadyState.CLOSED]: "Closed",
+		[ReadyState.UNINSTANTIATED]: "Uninstantiated",
+	}[readyState];
+
 	return (
 		<div className="content">
 			<div className="chat_div">
 				<div className="chat_main">
 					<p className="left">left</p>
 					<p className="right">right</p>
+					<ul>
+						{messageHistory.map((message, idx) => (
+							<span key={idx}>
+								{message ? message.data : null}
+							</span>
+						))}
+					</ul>
 				</div>
-				<form className="chat_input">
+				<div className="chat_input">
 					<input type="text" />
 					<div className="chat_send_Button">
-						<button>send</button>
+						<button onClick={handleClickSendMessage}>send</button>
 					</div>
-				</form>
+				</div>
 			</div>
 		</div>
 	);
